fa034dbdafd6a149d50f2bde9c024dc1cc64290f
fredli4qooni <fredlifourqoni999@gmail.com>
2025-12-05 16:41:49 +0700

    fix: set accept-encoding to identity to prevent decompression error on vercel 403 response

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,7 +5,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    "Accept-Encoding": "gzip,deflate,compress", 
+    "Accept-Encoding": "identity", 
   },
   timeout: 10000,
 });
