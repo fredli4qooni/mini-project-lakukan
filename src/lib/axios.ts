859aa18e023d84551a56e6094572ab95a19bce02
fredli4qooni <fredlifourqoni999@gmail.com>
2025-12-05 16:47:05 +0700

    chore: revert axios config to default standard

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,8 +4,6 @@ const api = axios.create({
   baseURL: "https://fakestoreapi.com",
   headers: {
     "Content-Type": "application/json",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    "Accept-Encoding": "identity", 
   },
   timeout: 10000,
 });
